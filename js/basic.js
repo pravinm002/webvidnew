4b03f993ca9e443086a845851ac0ad1cc5fc3f60
pravinm002 <pravinm002@gmail.com>
2024-10-21 14:16:42 +0530

    test

---
+++ js/basic.js
@@ -0,0 +1,359 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+
+// Global objects
+var avatarSynthesizer
+var peerConnection
+var previousAnimationFrameTimestamp = 0;
+"use strict";
+// Logger
+const log = msg => {
+    document.getElementById('logging').innerHTML += msg + '<br>'
+}
+
+// Setup WebRTC
+function setupWebRTC(iceServerUrl, iceServerUsername, iceServerCredential) {
+    console.log("inside setupWEBRTC")
+    // Create WebRTC peer connection
+    peerConnection = new RTCPeerConnection({
+        iceServers: [{
+            urls: [ iceServerUrl ],
+            username: iceServerUsername,
+            credential: iceServerCredential
+        }]
+    })
+
+    // Fetch WebRTC video stream and mount it to an HTML video element
+    peerConnection.ontrack = function (event) {
+        console.log("Received track: ", event.track.kind);
+        // Clean up existing video element if there is any
+        remoteVideoDiv = document.getElementById('remoteVideo')
+        for (var i = 0; i < remoteVideoDiv.childNodes.length; i++) {
+            if (remoteVideoDiv.childNodes[i].localName === event.track.kind) {
+                remoteVideoDiv.removeChild(remoteVideoDiv.childNodes[i])
+            }
+        }
+
+        const mediaPlayer = document.createElement(event.track.kind)
+        mediaPlayer.id = event.track.kind
+        mediaPlayer.srcObject = event.streams[0]
+        mediaPlayer.autoplay = true
+        document.getElementById('remoteVideo').appendChild(mediaPlayer)
+        document.getElementById('videoLabel').hidden = true
+        document.getElementById('overlayArea').hidden = false
+
+        if (event.track.kind === 'video') {
+            mediaPlayer.playsInline = true
+            remoteVideoDiv = document.getElementById('remoteVideo')
+            canvas = document.getElementById('canvas')
+           // if (document.getElementById('transparentBackground').checked) {
+                remoteVideoDiv.style.width = '0.1px'
+                canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+                canvas.hidden = false
+           // } else {
+           //     canvas.hidden = true
+           // }
+
+            mediaPlayer.addEventListener('play', () => {
+                //if (document.getElementById('transparentBackground').checked) {
+                    window.requestAnimationFrame(makeBackgroundTransparent)
+               // } else {
+                //    remoteVideoDiv.style.width = mediaPlayer.videoWidth / 2 + 'px'
+                //}
+            })
+        }
+        else
+        {
+            // Mute the audio player to make sure it can auto play, will unmute it when speaking
+            // Refer to https://developer.mozilla.org/en-US/docs/Web/Media/Autoplay_guide
+            mediaPlayer.muted = true
+        }
+    }
+
+    // Listen to data channel, to get the event from the server
+    peerConnection.addEventListener("datachannel", event => {
+        const dataChannel = event.channel
+        dataChannel.onmessage = e => {
+            console.log("[" + (new Date()).toISOString() + "] WebRTC event received: " + e.data)
+        }
+    })
+
+    // This is a workaround to make sure the data channel listening is working by creating a data channel from the client side
+    c = peerConnection.createDataChannel("eventChannel")
+
+    // Make necessary update to the web page when the connection state changes
+    peerConnection.oniceconnectionstatechange = e => {
+        console.log("ICE Connection State: " + peerConnection.iceConnectionState);
+        log("WebRTC status: " + peerConnection.iceConnectionState)
+
+        if (peerConnection.iceConnectionState === 'connected') {
+            document.getElementById('stopSession').disabled = false
+            document.getElementById('speak').disabled = false
+            document.getElementById('configuration').hidden = true
+        }
+
+        if (peerConnection.iceConnectionState === 'disconnected' || peerConnection.iceConnectionState === 'failed') {
+            document.getElementById('speak').disabled = true
+            document.getElementById('stopSpeaking').disabled = true
+            document.getElementById('stopSession').disabled = true
+            // document.getElementById('startSession').disabled = false
+            document.getElementById('configuration').hidden = false
+        }
+    }
+
+    // Offer to receive 1 audio, and 1 video track
+    peerConnection.addTransceiver('video', { direction: 'sendrecv' })
+    peerConnection.addTransceiver('audio', { direction: 'sendrecv' })
+
+    // start avatar, establish WebRTC connection
+    avatarSynthesizer.startAvatarAsync(peerConnection).then((r) => {
+        if (r.reason === SpeechSDK.ResultReason.SynthesizingAudioCompleted) {
+            console.log("Avatar started successfully.");
+            console.log("[" + (new Date()).toISOString() + "] Avatar started. Result ID: " + r.resultId)
+        } else {
+            console.error("Avatar failed to start. Reason:", r.reason);
+            console.log("[" + (new Date()).toISOString() + "] Unable to start avatar. Result ID: " + r.resultId)
+            if (r.reason === SpeechSDK.ResultReason.Canceled) {
+              
+                var cancellationDetails = SpeechSDK.CancellationDetails.fromResult(r)
+                if (cancellationDetails.reason === SpeechSDK.CancellationReason.Error) {
+                    console.log(cancellationDetails.errorDetails)
+                };
+                log("Unable to start avatar: " + cancellationDetails.errorDetails);
+            }
+            // document.getElementById('startSession').disabled = false;
+            document.getElementById('configuration').hidden = false;
+        }
+    }).catch(
+        (error) => {
+            console.log("[" + (new Date()).toISOString() + "] Avatar failed to start. Error: " + error)
+            // document.getElementById('startSession').disabled = false
+            document.getElementById('configuration').hidden = false
+        }
+    );
+}
+
+// Make video background transparent by matting
+function makeBackgroundTransparent(timestamp) {
+    console.log("inside makeBackgroundTransparent function")
+    // Throttle the frame rate to 30 FPS to reduce CPU usage
+    if (timestamp - previousAnimationFrameTimestamp > 30) {
+        video = document.getElementById('video')
+        tmpCanvas = document.getElementById('tmpCanvas')
+        tmpCanvas.backgroundColor = "#00FF00FF"
+        tmpCanvasContext = tmpCanvas.getContext('2d', { willReadFrequently: true })
+        tmpCanvasContext.drawImage(video, 0, 0, video.videoWidth, video.videoHeight)
+        if (video.videoWidth > 0) {
+            var frame = tmpCanvasContext.getImageData(0, 0, video.videoWidth, video.videoHeight)
+            for (var i = 0; i < frame.data.length / 4; i++) {
+                var r = frame.data[i * 4 + 0]
+                var g = frame.data[i * 4 + 1]
+                var b = frame.data[i * 4 + 2]
+                if (g - 150 > r + b) {
+                    // Set alpha to 0 for pixels that are close to green
+                    frame.data[i * 4 + 3] = 0
+                } else if (g + g > r + b) {
+                    // Reduce green part of the green pixels to avoid green edge issue
+                    adjustment = (g - (r + b) / 2) / 3
+                    r += adjustment
+                    g -= adjustment * 2
+                    b += adjustment
+                    frame.data[i * 4 + 0] = r
+                    frame.data[i * 4 + 1] = g
+                    frame.data[i * 4 + 2] = b
+                    // Reduce alpha part for green pixels to make the edge smoother
+                    a = Math.max(0, 255 - adjustment * 4)
+                    frame.data[i * 4 + 3] = a
+                }
+            }
+
+            canvas = document.getElementById('canvas')
+            canvasContext = canvas.getContext('2d')
+            canvasContext.putImageData(frame, 0, 0);
+        }
+
+        previousAnimationFrameTimestamp = timestamp
+    }
+
+    window.requestAnimationFrame(makeBackgroundTransparent)
+}
+// Do HTML encoding on given text
+function htmlEncode(text) {
+    const entityMap = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#39;',
+        '/': '&#x2F;'
+    };
+
+    return String(text).replace(/[&<>"'\/]/g, (match) => entityMap[match])
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    setTimeout(() => {
+       
+        startSession();
+    }, 5000);
+    
+//    startSession()
+    setTimeout(() => {
+        speak();
+    }, 10000);
+});
+
+function startSession ()  {
+  
+    console.log("inside function")
+    const cogSvcRegion = "westus2"
+    const cogSvcSubKey = "e5ac8f772ced4fd9822a5178e7966981"
+    if (cogSvcSubKey === '') {
+        alert('Please fill in the API key of your speech resource.')
+        return
+    }
+
+    const privateEndpointEnabled = document.getElementById('enablePrivateEndpoint').checked
+    const privateEndpoint = ""
+    if (privateEndpointEnabled && privateEndpoint === '') {
+        alert('Please fill in the Azure Speech endpoint.')
+        return
+    }
+
+    var speechSynthesisConfig
+    if (privateEndpointEnabled) {
+        speechSynthesisConfig = SpeechSDK.SpeechConfig.fromEndpoint(new URL(`wss://${privateEndpoint}/tts/cognitiveservices/websocket/v1?enableTalkingAvatar=true`), cogSvcSubKey) 
+    } else {
+        speechSynthesisConfig = SpeechSDK.SpeechConfig.fromSubscription(cogSvcSubKey, cogSvcRegion)
+    }
+    speechSynthesisConfig.endpointId = ""
+
+    const videoFormat = new SpeechSDK.AvatarVideoFormat()
+    var videoCropTopLeftX = document.getElementById('videoCrop').checked ? 600 : 0
+    var videoCropBottomRightX = document.getElementById('videoCrop').checked ? 1320 : 1920
+    videoFormat.setCropRange(new SpeechSDK.Coordinate(videoCropTopLeftX, 0), new SpeechSDK.Coordinate(videoCropBottomRightX, 1080));
+
+    const talkingAvatarCharacter = "Lisa"
+    const talkingAvatarStyle = "casual-sitting"
+    const avatarConfig = new SpeechSDK.AvatarConfig(talkingAvatarCharacter, talkingAvatarStyle, videoFormat)
+    avatarConfig.customized = document.getElementById('customizedAvatar').checked
+    avatarConfig.backgroundColor = "#00FF00FF"
+    avatarSynthesizer = new SpeechSDK.AvatarSynthesizer(speechSynthesisConfig, avatarConfig)
+    avatarSynthesizer.avatarEventReceived = function (s, e) {
+        var offsetMessage = ", offset from session start: " + e.offset / 10000 + "ms."
+        if (e.offset === 0) {
+            offsetMessage = ""
+        }
+        console.log("[" + (new Date()).toISOString() + "] Event received: " + e.description + offsetMessage)
+    }
+
+    // document.getElementById('startSession').disabled = true
+    
+    const xhr = new XMLHttpRequest()
+    if (privateEndpointEnabled) {
+        xhr.open("GET", `https://${privateEndpoint}/tts/cognitiveservices/avatar/relay/token/v1`)
+    } else {
+        xhr.open("GET", `https://${cogSvcRegion}.tts.speech.microsoft.com/cognitiveservices/avatar/relay/token/v1`)
+    }
+    xhr.setRequestHeader("Ocp-Apim-Subscription-Key", cogSvcSubKey)
+    xhr.addEventListener("readystatechange", function() {
+        if (this.readyState === 4) {
+            console.log("Received WebRTC ICE server details: ", this.responseText);
+            const responseData = JSON.parse(this.responseText)
+            const iceServerUrl = responseData.Urls[0]
+            const iceServerUsername = responseData.Username
+            const iceServerCredential = responseData.Password
+            setupWebRTC(iceServerUrl, iceServerUsername, iceServerCredential)
+        }
+    })
+    xhr.send()
+    
+}
+
+var data = [
+    { "Hello Good Morning": "Hi Good Morning" },
+    { "How are you": "I am Good" }
+];
+function getResponse(spokenText) {
+    for (var i = 0; i < data.length; i++) {
+        var key = Object.keys(data[i])[0];
+        if (key === spokenText) {
+            return data[i][key];
+        }
+    }
+    return "Sorry, I don't understand."; // Default response if no match is found
+}
+
+
+
+window.speak = () => {
+  
+    document.getElementById('speak').disabled = true;
+    document.getElementById('stopSpeaking').disabled = false
+    //document.getElementById('audio').muted = false
+  
+    var spokenText = "How are you";
+    var responseText = getResponse(spokenText);
+    console.log(responseText); 
+
+    var ttsVoice = document.getElementById('ttsVoice').value
+    var personalVoiceSpeakerProfileID = document.getElementById('personalVoiceSpeakerProfileID').value
+    var spokenSsml = `<speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xmlns:mstts='http://www.w3.org/2001/mstts' xml:lang='en-US'><voice name='${ttsVoice}'><mstts:ttsembedding speakerProfileId='${personalVoiceSpeakerProfileID}'><mstts:leadingsilence-exact value='0'/>${htmlEncode(responseText)}</mstts:ttsembedding></voice></speak>`
+    console.log("[" + (new Date()).toISOString() + "] Speak request sent.")
+    avatarSynthesizer.speakSsmlAsync(spokenSsml).then(
+        (result) => {
+            document.getElementById('speak').disabled = false
+            document.getElementById('stopSpeaking').disabled = true
+            if (result.reason === SpeechSDK.ResultReason.SynthesizingAudioCompleted) {
+                console.log("[" + (new Date()).toISOString() + "] Speech synthesized to speaker for text [ " + responseText + " ]. Result ID: " + result.resultId)
+            } else {
+                console.log("[" + (new Date()).toISOString() + "] Unable to speak text. Result ID: " + result.resultId)
+                if (result.reason === SpeechSDK.ResultReason.Canceled) {
+                    var cancellationDetails = SpeechSDK.CancellationDetails.fromResult(result)
+                    console.log(cancellationDetails.reason)
+                    if (cancellationDetails.reason === SpeechSDK.CancellationReason.Error) {
+                        console.log(cancellationDetails.errorDetails)
+                    }
+                }
+            }
+        }).catch(log);
+}
+
+
+window.stopSpeaking = () => {
+    document.getElementById('stopSpeaking').disabled = true
+
+    avatarSynthesizer.stopSpeakingAsync().then(
+        log("[" + (new Date()).toISOString() + "] Stop speaking request sent.")
+    ).catch(log);
+}
+
+window.stopSession = () => {
+    document.getElementById('speak').disabled = true
+    document.getElementById('stopSession').disabled = true
+    document.getElementById('stopSpeaking').disabled = true
+    avatarSynthesizer.close()
+    console.log("stopsession")
+}
+
+window.updataTransparentBackground = () => {
+    //if (document.getElementById('transparentBackground').checked) {
+        document.body.background = './image/background.png'
+        document.getElementById('backgroundColor').value = '#00FF00FF'
+        document.getElementById('backgroundColor').disabled = true
+        console.log("updataTransparentBackground")
+   // } else {
+    //    document.body.background = ''
+    //    document.getElementById('backgroundColor').value = '#FFFFFFFF'
+    //    document.getElementById('backgroundColor').disabled = false
+   // }
+}
+
+window.updatePrivateEndpoint = () => {
+    if (document.getElementById('enablePrivateEndpoint').checked) {
+        document.getElementById('showPrivateEndpointCheckBox').hidden = false
+    } else {
+        document.getElementById('showPrivateEndpointCheckBox').hidden = true
+    }
+}
